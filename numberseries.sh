9e22e86616ecbf9b121d9340b8bcf726fc9f9c3d
“FaizanMomin1” <faizanmomin8991@gmail.com>
2023-03-07 09:51:31 +0530

    Add: UC3- Branch store in dictionary

--- numberseries.sh
+++ numberseries.sh
@@ -1,8 +1,9 @@
 echo "Welcome to Number Series"
+declare -a dict
 for i in {1..10}
 do
   echo $i
 done
 arr=(1 2 4 7 9 10)
-echo "Array contents: ${arr[@]}"
-
+dict=${arr[@]}
+echo "Selected Number is" $dict
